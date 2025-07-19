001d7627be190574e0e61ef5584762f9ef98a4f0
Aditi <aditi786aaa@gmail.com>
2025-07-19 21:36:36 +0530

    update

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,6 +4,7 @@ from .db_connect import get_connection
 def insert_data(filepath, city):
     df = pd.read_csv(filepath)
     df.columns = df.columns.str.strip()  # in case of whitespaces
+    df = df.replace({pd.NA: None, 'nan': None, 'NaN': None, '': None})
     df = df.where(pd.notnull(df), None)  # convert NaN to None
 
     conn = get_connection()
